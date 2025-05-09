8d210e8e6798ed01f9f3ae43207e447193fd4f79
alexandre-niess <alexandrefersoa@gmail.com>
2025-05-08 23:07:00 -0300

    FEAT: Tela pagamento

---
+++ screens/Pagamento.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Header from "../components/Header";
+
+export function Pagamento() {
+  return (
+    <>
+      <CssBaseline />
+      <Header headerType="pagamento" />
+    </>
+  );
+}
+
+export default Pagamento;
